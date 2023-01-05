90527110d5d4f1f87bd1d06dd28554f816b3c54e
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-01-05 14:24:20 +0530

    Create A4Q3.py

---
+++ A4Q3.py
@@ -0,0 +1,20 @@
+import random
+
+# generate ten random multiplication questions
+for i in range(10):
+   # generate two random numbers between 1 and 9
+   num1 = random.randint(1, 9)
+   num2 = random.randint(1, 9)
+
+   # compute the correct answer
+   answer = num1 * num2
+
+   # ask the player to solve the question
+   guess = input(f"Question {i+1}: {num1} x {num2} = ")
+   guess = int(guess)
+
+   # check if the player's guess is correct
+   if guess == answer:
+       print("Right!")
+   else:
+       print(f"Wrong. The answer is {answer}.")
