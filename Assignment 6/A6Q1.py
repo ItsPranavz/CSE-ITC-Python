0c7af39abee8b06a746b154711a2cc62b4e94e11
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-02 18:44:20 +0530

    Update and rename A6Q1.py to Assignment 6/A6Q1.py

--- Assignment 6/A6Q1.py
+++ Assignment 6/A6Q1.py
@@ -1,11 +1,3 @@
-'''Write a Python function to check whether a number is perfect or not. According to
-Wikipedia : In number theory, a perfect number is a positive integer that is equal to the
-sum of its proper positive divisors, that is, the sum of its positive divisors excluding the
-number itself (also known as its aliquot sum). Equivalently, a perfect number is a
-number that is half the sum of all of its positive divisors (including itself).'''
-
-
-
 def check_perfect(n):                          #defining a function which returns True if n is perfect and False if not
     list1=[]                                   #list of divisors of n, currently empty
     for i in range (1,n):                      #loop runs from 1 to n-1
