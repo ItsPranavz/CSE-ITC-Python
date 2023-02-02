ff735c41c75d9f6770430d11a0e95e5dc314cd92
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-02 18:46:06 +0530

    Update and rename A6Q6.py to Assignment 6/A6Q6.py

--- Assignment 6/A6Q6.py
+++ Assignment 6/A6Q6.py
@@ -1,7 +1,3 @@
-'''Write a Python function student_data () which will print the id of a student (student_id).
-If the user passes an argument student_name or student_class the function will print the
-student name and class.'''
-
 def student_data(student_id,**kwargs):
     print("Student ID is ",student_id)
     if 'student_name' in kwargs:
