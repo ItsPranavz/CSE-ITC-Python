6ebddccb75997d89aeb437cd1381e4742dc30958
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-02 18:46:56 +0530

    Update and rename A6Q8.py to Assignment 6/A6Q8.py

--- Assignment 6/A6Q8.py
+++ Assignment 6/A6Q8.py
@@ -1,8 +1,3 @@
-''''Write a Python class to find the three elements that sum to zero from a set of n real
-numbers.
-Input array: [-25, -10, -7, -3, 2, 4, 8, 10]
-Output: [[-10, 2, 8], [-7, -3, 10]]'''
-
 class Sum0:
     def output():
         n=int(input("Enter the number of elements in the input array"))
