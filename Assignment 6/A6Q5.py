268dc5bf2a03af252e63b6c3b3e32f45e3d98531
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-02 18:45:47 +0530

    Update and rename A6Q5.py to Assignment 6/A6Q5.py

--- Assignment 6/A6Q5.py
+++ Assignment 6/A6Q5.py
@@ -1,8 +1,3 @@
-'''Write a Python function that accepts a hyphen-separated sequence of words as input
-and prints the words in a hyphen-separated sequence after sorting them alphabetically.
-Sample Items : green-red-yellow-black-white
-Expected Result : black-green-red-white-yellow'''
-
 str1=input("Enter the hyphen-separated sequence of words\n")      #taking the input string
 list1=str1.split('-')                                           #extracting the words into a list
 list1=sorted(list1)                                             #sorting the words
